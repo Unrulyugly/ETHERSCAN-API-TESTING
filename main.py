8d58f24ddd41aa5fdd07e636fb2083e4475209d6
CodeSandbox <support@codesandbox.io>
2024-01-02 12:57:41 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello CodeSandbox!")
+# define a function to check if a number is prime
+def prime():
+    n = int(input("Enter the number? "))
+    if n / n == 1:
+        print("The number", n, "is a prime number")
+    prime()
